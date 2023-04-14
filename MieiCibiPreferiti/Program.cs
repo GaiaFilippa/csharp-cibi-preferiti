88ae8f4bf81476bc4287a7696ba360efd72b65a6
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-04-14 14:01:55 +0200

    Aggiungere i file di progetto.

---
+++ MieiCibiPreferiti/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Create un progetto App Console CibiPreferiti con Visual Studio.
+ * Inizializza un array con la classifica dei miei cibi preferiti (minimo 5, massimo 10 elementi). HARD-CODED
+ * L’array deve essere già inizializzato nel programma ordinato già con i cibi dal più preferito al meno preferito.
+ * 
+ * Stampa a video le seguenti informazioni:
+ * La lunghezza della classifica
+ * La classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
+ * Il mio cibo top (prima posizione della classifica)
+ * Il mio cibo preferito ma non troppo (ultima posizione della classifica)
+ */
