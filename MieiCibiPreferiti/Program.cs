de93e481733b702e0c33c2acbfdc58b4ba1f4edc
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-04-14 17:33:16 +0200

    functions classific

--- MieiCibiPreferiti/Program.cs
+++ MieiCibiPreferiti/Program.cs
@@ -8,3 +8,47 @@
  * Il mio cibo top (prima posizione della classifica)
  * Il mio cibo preferito ma non troppo (ultima posizione della classifica)
  */
+
+
+// MAIN ----------------------------------------------------------
+
+string[] arrayMyFavoriteFood = { "Polenta", "Risotto", "Pizza", "Bunet", "Ravioli al vapore", "Fonduta"};
+
+Console.WriteLine("La lunghezza della classifica è: " + getArrayLenght(arrayMyFavoriteFood));
+
+Console.WriteLine();
+
+Console.WriteLine("La mia classifica dei cibi preferiti è: ");
+printArrayString(arrayMyFavoriteFood);
+
+Console.WriteLine();
+
+Console.Write("Il mio cibo top è: ");
+getFirstFavFood(arrayMyFavoriteFood);
+
+
+
+// FUNZIONI ------------------------------------------------------
+
+int getArrayLenght(string[] arrayString)
+{
+    return arrayString.Length;
+}
+
+void printArrayString(string[] arrayString)
+{
+    for (int i = 0; i < arrayString.Length; i++)
+    {
+        Console.WriteLine((i + 1) + "° " + arrayString[i]);
+    }
+}
+
+void getFirstFavFood(string[] firstFood) {
+    for (int i = 0; i < firstFood.Length; i++)
+    {
+        if (i == 0) {
+            Console.WriteLine();
+        }
+    }
+    
+}
